ed5af9e915271f1f52dedef3fb131bec0bfaa1ac
Qu123e3 <buiquangthanh1129@gmail.com>
2023-06-10 10:29:26 +0700

    thaydoi

--- 1.cpp
+++ 1.cpp
@@ -1,12 +1,15 @@
 #include <stdio.h>
-int main(){
+int main()
+{
     int n;
     int S = 0;
     printf("\nNhap n =  ");
     scanf("%d", &n);
-    for(int i = 1; i <= n; i++)
+    for (int i = 1; i <= n; i++)
     {
         S += i;
     }
-    printf("\nS = %d", S);
+    mbnvmabj
+
+        printf("\nS = %d", S);
 }
